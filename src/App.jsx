d623ae4b5005d679ef4a5f1d2adafc8582534f69
Marcelo Gutiérrez <jmezvo@gmail.com>
2022-10-08 23:21:37 -0500

    CRUD Users List completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
-import './reset.css';
+import ModalComponent from './components/ModalComponent';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Button } from 'react-bootstrap';
 
-function App() {
+const App = () => {
+  const [usersList, setUsersList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://users-crud1.herokuapp.com/users/')
+      .then(res => setUsersList(res.data))
+      .finally(() => console.log('Response Finalized'));
+  }, []);
+
+  console.log('Users List: ', usersList);
+
   return (
     <div className='App'>
-      <h1>¡Hola Mundo!</h1>
+      {/* MODAL COMPONENT */}
+      <ModalComponent />
+
+      {/* USERS LIST */}
+      <section>
+        <ul>
+          {usersList.map(user => (
+            <li key={user.id}>
+              <div>
+                <h3>
+                  {user.first_name} {user.last_name}
+                </h3>
+                <p>Correo:</p>
+                <p>{user.email}</p>
+                <p>Birthday:</p>
+                <p>{user.birthday}</p>
+              </div>
+              <div>
+                <Button variant='primary'>
+                  <i className='fa-solid fa-pencil'></i>
+                </Button>
+                <Button variant='danger'>
+                  <i className='fa-solid fa-trash-can'></i>
+                </Button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ModalComponent.jsx
@@ -0,0 +1,102 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Button, Col, Form, Modal, Row } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+
+const ModalComponent = () => {
+  const [show, setShow] = useState(false);
+  const { register, handleSubmit } = useForm();
+
+  const submit = data => {
+    axios
+      .post('https://users-crud1.herokuapp.com/users/', data)
+      .catch(err => err.response);
+    handleClose();
+  };
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant='primary' onClick={handleShow}>
+        <i className='fa-solid fa-user-plus'></i>
+        <span> Create new user</span>
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>New User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Row className='mb-3'>
+              <Form.Group as={Col} controlId='formGridName'>
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  autoFocus
+                  required
+                  type='text'
+                  placeholder='Enter First Name'
+                  {...register('first_name')}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId='formGridLastName'>
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  required
+                  type='text'
+                  placeholder='Enter Last Name'
+                  {...register('last_name')}
+                />
+              </Form.Group>
+            </Row>
+            <Form.Group className='mb-3' controlId='formGridEmail'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                required
+                type='email'
+                placeholder='Example@mail.com'
+                {...register('email')}
+              />
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formGridPassword'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                required
+                type='password'
+                placeholder='Enter Password'
+                {...register('password')}
+              />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='formGridBirthday'>
+              <Form.Label>Birthday</Form.Label>
+              <Form.Control
+                required
+                type='date'
+                placeholder='Enter Birthday'
+                {...register('birthday')}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant='primary'
+            type='submit'
+            onClick={handleSubmit(submit)}
+          >
+            <i className='fa-solid fa-floppy-disk'></i> <span> Save</span>
+          </Button>
+          <Button variant='secondary' onClick={handleClose}>
+            <i className='fa-solid fa-xmark'></i> <span> Close</span>
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalComponent;
